5c9c447e7bea470e1756e1fa938604bc9aa92514
xiyongjian <xiyongjian@hotmail.com>
2018-01-15 12:05:29 -0700

    Update zipline_run_local_data.py

--- zipline_run_local_data.py
+++ zipline_run_local_data.py
@@ -1,3 +1,46 @@
+########################################### simple version ###############################
+import pandas as pd
+import locale
+import matplotlib.pyplot as plt
+from zipline.algorithm import TradingAlgorithm
+import zipline.finance.trading as trading
+# from zipline.utils.factory import load_from_yahoo
+from datetime import datetime, date
+from pytz import timezone
+import pytz
+from dateutil.tz import tzutc
+from dateutil.parser import parse
+from zipline.finance.slippage import FixedSlippage
+from zipline.finance.commission import PerShare, PerTrade
+
+from pandas_datareader.google.daily import GoogleDailyReader
+
+@property
+def url(self):
+    print("call @property get url, %s, %s"%(self, type(self)))
+    return 'http://finance.google.com/finance/historical'
+
+GoogleDailyReader.url = url
+
+
+
+# Define algorithm
+def initialize(context):
+    pass
+
+def handle_data(context, data):
+    print("handle_data : %s"%data.current_dt)
+
+# Create algorithm object passing in initialize, handle_data functions
+algo_obj = TradingAlgorithm(initialize=initialize, handle_data=handle_data)
+
+perf_manual = algo_obj.run(bars)
+
+
+########################################### long version ###############################
+
+
+
 import pandas as pd
 import locale
 import matplotlib.pyplot as plt
